cd1ea2564e5e96cb04b175fced8df5218a976d2a
ramonfvasconcelos <rfernandesvasc@gmail.com>
2026-05-30 19:02:24 -0300

    alterações nos espaços

--- test/servicoDePagamento.test.js
+++ test/servicoDePagamento.test.js
@@ -25,11 +25,9 @@ describe('Classe de Serviço de Pagamento', function() {
     it('Validar que a categoria seja cara para pagamento acima de 100.00', () => {
         //Arrange
         const servicoPagamento = new ServicoDePagamento();
-
         //Act
         servicoPagamento.pagar('0987-7656-3475', 'Energia', 140.00);
         const ultimoPagamento = servicoPagamento.consultarUltimoPagamento();
-
         //Assert
         assert.equal(ultimoPagamento.categoria, 'cara');
     });
@@ -37,11 +35,9 @@ describe('Classe de Serviço de Pagamento', function() {
     it('Validar que o objeto retornado contém todas as propriedades esperadas', () => {
         //Arrange
         const servicoPagamento = new ServicoDePagamento();
-
         //Act
         servicoPagamento.pagar('0987-7656-3475', 'Energia', 140.00);
         const ultimoPagamento = servicoPagamento.consultarUltimoPagamento();
-
         //Assert
         assert.equal(ultimoPagamento.codigoBarras, '0987-7656-3475');
         assert.equal(ultimoPagamento.empresa, 'Energia');
@@ -53,13 +49,11 @@ describe('Classe de Serviço de Pagamento', function() {
     it('Validar que após vários pagamentos a consulta retorne somente o mais recente', () => {
         //Arrange
         const servicoPagamento = new ServicoDePagamento();
-
         //Act
         servicoPagamento.pagar('0987-7656-3475', 'Energia', 140.00);
         servicoPagamento.pagar('1234-5678-9000', 'Condomínio', 100.00);
         servicoPagamento.pagar('0987-7656-3475', 'Internet', 99.99);
         const ultimoPagamento = servicoPagamento.consultarUltimoPagamento();
-
         //Assert
         assert.equal(ultimoPagamento.codigoBarras, '0987-7656-3475');
         assert.equal(ultimoPagamento.empresa, 'Internet');
